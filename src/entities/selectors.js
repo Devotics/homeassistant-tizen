f23e709133894654101c3b6fa8e1712ac8a63429
Antoine Colmard <a.colmard@gmail.com>
2018-10-21 01:55:18 +0200

    :hammer: Keep entity order

--- src/entities/selectors.js
+++ src/entities/selectors.js
@@ -2,6 +2,7 @@ import { createSelector } from 'reselect';
 
 const isVisible = entity => !entity.attributes.hidden;
 export const selectState = state => state.entities;
-export const selectEntities = createSelector(selectState, entities => Object.values(entities));
+// eslint-disable-next-line max-len
+export const selectEntities = createSelector(selectState, entities => Object.values(entities).sort((a, b) => a.entity_id.localeCompare(b.entity_id)));
 // eslint-disable-next-line max-len
 export const selectVisibleEntities = createSelector(selectEntities, entities => entities.filter(isVisible));
